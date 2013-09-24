06b28ba23cf20ee00c866d04832ac740e0524a99
Jenton <addie@Adriennes-MacBook-Air.local>
2013-09-23 23:30:20 -0700

    fixed the if, N/A statements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
   
-        var API_KEY = 'dj0yJmk9RkxOQUtxdk9uQXFNJmQ9WVdrOWVETjZNMVIzTjJjbWNHbzlPVFkxTXpVMU5UWXkmcz1jb25zdW1lcnNlY3JldCZ4PTdk';
-        
-        $(document).ready(function() {
+var API_KEY = 'dj0yJmk9RkxOQUtxdk9uQXFNJmQ9WVdrOWVETjZNMVIzTjJjbWNHbzlPVFkxTXpVMU5UWXkmcz1jb25zdW1lcnNlY3JldCZ4PTdk';
+
+$(document).ready(function() {
             /*var dataObj = {
                 api_key : API_KEY,
                 method : 'flickr.photos.search',
@@ -24,44 +24,52 @@
                 url: 'http://feeds.delicious.com/v2/json/recent',
                 data: dataObj,
                 dataType: 'jsonp',
-                })
+            })
             .done(function(data){
                 //alert("success");
                 var info = data;
                 //console.log(info);
-                for (var i = 0; i < 20; i++) {
+                for (var i = 0; i < 40; i++) {
                     var test = info[i];
                     var div = buildDiv(test.a, test.d, test.u, test.n, test.t);
+                    console.log(test.t);
+                    //console.log(test.t.length);
                     $('#gallery').append(div);
                     //console.log(test);
                 }
                 console.log("Done");
-                    })
+            })
             .fail(function(xhr, e) {
                 console.log(e);
             });                     
         });         
-         
+
         //this function is not used yet 
         function buildLink(farm, server, id, secret) {
           return 'http://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '.jpg'    
-        }
-        
-        function buildDiv(user, title, link, text, tags) {
-           //This replaces any null values with 'N/A'
-           if (user == '') {
-            user = 'N/A';
-            } else if (title == '') {
-                title = 'N/A';
-            } else if (link == '') {
-                link = 'N/A';
-            } else if (text == '') {
-                text = 'N/A';
-            } else if (tags == '') {
-                tags = 'N/A';
-            }
+      }
 
-           var div = $('<div></div>')
-                        .append('<p>' + 'User: ' + user + ' | Title: ' + title + ' | URL: ' + link + ' | Text: ' + text + ' | Tags: ' + tags + '</p>');
-           return div;
+      function buildDiv(user, title, link, text, tags) {
+           //This replaces any null values with 'N/A'
+          // console.log(tags.length);
+          if (user == '') {
+            user = 'N/A';
+        } 
+        if (title == '') {
+            title = 'N/A';
+        } 
+        if (link == '') {
+            link = 'N/A';
+        } 
+        if (text == '') {
+            text = 'N/A';
+        } 
+        if (tags[0].length == 0) {
+            tags = 'N/A';
         }
+        console.log(tags[0].length);
+
+        var div = $('<div></div>')
+        .append('<p>' + 'User: ' + user + ' | Title: ' + title + ' | URL: ' + link + ' | Text: ' + text + ' | Tags: ' + tags + '</p>');
+        return div;
+    }
